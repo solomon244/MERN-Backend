64f9af6461d8c56789d040a8c764681590df65a4
solomon244 <slegesse2444@gmail.com>
2024-02-11 07:28:50 +0300

    adding auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1]; // Use the existing 'token' variable instead of redefining it with 'const'
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ error: "Not Authorized" });
+    }
+  } else {
+    res.status(401).json({ error: "No token provided" });
+  }
+});
+
+module.exports ={ protect};
